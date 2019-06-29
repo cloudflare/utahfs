453ffb495c0e1c4b51fcaecd8cb0c41ab128ec1e
Brendan McMillion <brendan@cloudflare.com>
2019-06-29 11:55:38 -0700

    Upgrade encryption to be a BlockStorage implementation.

--- persistent/encryption.go
+++ persistent/encryption.go
@@ -12,15 +12,15 @@ import (
 )
 
 type encryption struct {
-	base ObjectStorage
+	base BlockStorage
 	aead cipher.AEAD
 }
 
-// WithEncryption wraps an ObjectStorage implementation and makes sure that all
+// WithEncryption wraps a BlockStorage implementation and makes sure that all
 // values are encrypted with AES-GCM before being processed further.
 //
 // The encryption key is derived with PBKDF2 from `password`.
-func WithEncryption(base ObjectStorage, password string) (ObjectStorage, error) {
+func WithEncryption(base BlockStorage, password string) (BlockStorage, error) {
 	key := pbkdf2.Key([]byte(password), []byte("7fedd6d671beec56"), 4096, 32, sha1.New)
 
 	block, err := aes.NewCipher(key)
@@ -35,8 +35,10 @@ func WithEncryption(base ObjectStorage, password string) (ObjectStorage, error) 
 	return &encryption{base, aead}, nil
 }
 
-func (e *encryption) Get(ctx context.Context, key string) ([]byte, error) {
-	raw, err := e.base.Get(ctx, key)
+func (e *encryption) Start(ctx context.Context) error { return e.base.Start(ctx) }
+
+func (e *encryption) Get(ctx context.Context, ptr uint32) ([]byte, error) {
+	raw, err := e.base.Get(ctx, ptr)
 	if err != nil {
 		return nil, err
 	}
@@ -44,19 +46,18 @@ func (e *encryption) Get(ctx context.Context, key string) ([]byte, error) {
 	if len(raw) < ns {
 		return nil, fmt.Errorf("storage: ciphertext is too small")
 	}
-	return e.aead.Open(nil, raw[:ns], raw[ns:], []byte(key))
+	return e.aead.Open(nil, raw[:ns], raw[ns:], []byte(fmt.Sprintf("%x", ptr)))
 }
 
-func (e *encryption) Set(ctx context.Context, key string, data []byte) error {
+func (e *encryption) Set(ctx context.Context, ptr uint32, data []byte) error {
 	nonce := make([]byte, e.aead.NonceSize())
 	if _, err := rand.Read(nonce); err != nil {
 		return err
 	}
-	ct := e.aead.Seal(nil, nonce, data, []byte(key))
+	ct := e.aead.Seal(nil, nonce, data, []byte(fmt.Sprintf("%x", ptr)))
 
-	return e.base.Set(ctx, key, append(nonce, ct...))
+	return e.base.Set(ctx, ptr, append(nonce, ct...))
 }
 
-func (e *encryption) Delete(ctx context.Context, key string) error {
-	return e.base.Delete(ctx, key)
-}
+func (e *encryption) Commit(ctx context.Context) error { return e.base.Commit(ctx) }
+func (e *encryption) Rollback(ctx context.Context)     { e.base.Rollback(ctx) }
