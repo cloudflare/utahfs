d33a315845c24c1fd1a0c31eecaabcad1148e852
Brendan McMillion <brendan@cloudflare.com>
2019-07-22 13:37:08 -0700

    Fix WAL disk format.

--- persistent/local_wal.go
+++ persistent/local_wal.go
@@ -36,27 +36,48 @@ type transaction struct {
 }
 
 func readTransaction(r io.Reader) (*transaction, int, error) {
-	hdr := make([]byte, 12)
+	hdr := make([]byte, 17)
 	if _, err := io.ReadFull(r, hdr); err != nil {
 		return nil, 0, err
 	} else if hdr[0] != 0 && hdr[0] != 1 {
 		return nil, 0, fmt.Errorf("wal: data is malformed")
 	}
-	id, dataLen := readInt(hdr[1:9]), readInt(hdr[9:12])
+	id, dataLen := readInt(hdr[1:9]), readInt(hdr[9:17])
 
-	lim := &io.LimitedReader{r, int64(dataLen)}
-	data, err := readMap(lim)
-	if err == io.EOF || err == nil && lim.N != 0 {
-		return nil, 0, io.ErrUnexpectedEOF
-	} else if err != nil {
-		return nil, 0, err
+	var (
+		data map[string][]byte
+		err  error
+	)
+	if hdr[0] == 0 {
+		lim := &io.LimitedReader{r, int64(dataLen)}
+		data, err = readMap(lim)
+		if err == io.EOF || err == nil && lim.N != 0 {
+			return nil, 0, io.ErrUnexpectedEOF
+		} else if err != nil {
+			return nil, 0, err
+		}
+	} else {
+		// Advance the reader forward by `dataLen` amount without parsing any of
+		// the data. It's not needed, the tx has already been processed.
+		discard := make([]byte, 1024*1024)
+		for n := 0; n < dataLen; {
+			m := len(discard)
+			if n+m > dataLen {
+				m = dataLen - n
+			}
+			o, err := r.Read(discard[:m])
+			if err != nil {
+				return nil, 0, err
+			}
+			n += o
+		}
 	}
 
 	return &transaction{
 		Processed: hdr[0] == 1,
 		Id:        uint64(id),
 		Data:      data,
-	}, 12 + dataLen, nil
+	}, 17 + dataLen, nil
 }
 
 func writeTransaction(w io.Writer, tx *transaction) error {
@@ -65,12 +86,12 @@ func writeTransaction(w io.Writer, tx *transaction) error {
 		return err
 	}
 
-	hdr := make([]byte, 12)
+	hdr := make([]byte, 17)
 	if tx.Processed {
 		hdr[0] = 1
 	}
 	writeInt(int(tx.Id), hdr[1:9])
-	writeInt(buff.Len(), hdr[9:12])
+	writeInt(buff.Len(), hdr[9:17])
 
 	if _, err := w.Write(hdr); err != nil {
 		return err
@@ -147,7 +168,9 @@ func NewLocalWAL(base ObjectStorage, loc string, maxSize int) (ReliableStorage, 
 			return nil, err
 		}
 
-		currSize += len(t.Data)
+		if !t.Processed {
+			currSize += len(t.Data)
+		}
 
 		if !t.Processed && !foundQueue1 {
 			startQueue1 = pos
@@ -181,6 +204,16 @@ func NewLocalWAL(base ObjectStorage, loc string, maxSize int) (ReliableStorage, 
 	}
 	if !foundQueue2 {
 		startQueue2 = pos
+	}
+
+	if currSize == 0 {
+		if err := fh.Truncate(0); err != nil {
+			return nil, err
+		}
+		startQueue1 = 0
+		endQueue1 = 0
+		startQueue2 = 0
+		nextTx = 0
 	}
 
 	wal := &localWAL{
@@ -246,7 +279,7 @@ func (lw *localWAL) drainOnce() error {
 		lw.mu.Unlock() // mu was left locked by processTx.
 	}
 
-	// Full drain queue 2.
+	// Fully drain queue 2.
 	lw.mu.Lock()
 	pos := lw.startQueue2
 	lw.mu.Unlock()
@@ -313,7 +346,7 @@ func (lw *localWAL) processTx(pos int64, tx *transaction) error {
 	}
 
 	lw.mu.Lock()
-	// defer lw.mu.Unlock() -- Intentionally left unlocked.
+	// defer lw.mu.Unlock() -- Intentionally left locked.
 
 	// Mark this transaction as processed.
 	if _, err := lw.fh.Seek(pos, io.SeekStart); err != nil {
@@ -406,25 +439,25 @@ func (lw *localWAL) Commit(ctx context.Context, writes map[string][]byte) error 
 	id := lw.nextTx
 	pos := int64(0)
 
-	buff1 := &bytes.Buffer{}
-	if err := writeTransaction(buff1, &transaction{false, id, writes}); err != nil {
-		return err
-	}
-	buff2 := &bytes.Buffer{}
-	if err := writeTransaction(buff2, &transaction{true, 0, nil}); err != nil {
+	buff := &bytes.Buffer{}
+	if err := writeTransaction(buff, &transaction{false, id, writes}); err != nil {
 		return err
 	}
 
-	if lw.startQueue2-lw.endQueue1 > int64(buff1.Len()+buff2.Len()) {
+	if gap := int(lw.startQueue2 - lw.endQueue1); gap > buff.Len()+17 {
 		// Add this data at the end of queue 1, since there's enough room.
 		pos = lw.endQueue1
-		endPos := lw.endQueue1 + int64(buff1.Len())
+		endPos := lw.endQueue1 + int64(buff.Len())
+
+		emptyTx := make([]byte, 17)
+		emptyTx[0] = 1
+		writeInt(gap-buff.Len()-17, emptyTx[9:17])
 
 		if _, err := lw.fh.Seek(pos, io.SeekStart); err != nil {
 			return err
-		} else if _, err := buff1.WriteTo(lw.fh); err != nil { // Write the actual data.
+		} else if _, err := buff.WriteTo(lw.fh); err != nil { // Write the actual data.
 			return err
-		} else if _, err := buff2.WriteTo(lw.fh); err != nil { // Write an empty transaction to mark the end of queue1.
+		} else if _, err := lw.fh.Write(emptyTx); err != nil { // Write an empty transaction to mark the end of queue1.
 			return err
 		}
 
@@ -434,7 +467,7 @@ func (lw *localWAL) Commit(ctx context.Context, writes map[string][]byte) error 
 		var err error
 		if pos, err = lw.fh.Seek(0, io.SeekEnd); err != nil {
 			return err
-		} else if _, err := buff1.WriteTo(lw.fh); err != nil {
+		} else if _, err := buff.WriteTo(lw.fh); err != nil {
 			return err
 		}
 	}
